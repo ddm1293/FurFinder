f948a74c8b58c3cc5b0517634cf768ef7155e744
hanli <lihanmun61@gmail.com>
2023-06-01 16:54:05 -0700

    Initial Navbar

---
+++ frontend/src/Navbar/RightMenu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Menu, Avatar } from "antd";
+import { UserOutlined, CodeOutlined, LogoutOutlined } from "@ant-design/icons";
+
+const RightMenu = ({ mode }) => {
+    return (
+        <Menu mode={mode}>
+            <Menu.SubMenu
+                title={
+                    <>
+                        <Avatar icon={<UserOutlined />} />
+                        <span className="username">Marry Browns</span>
+                    </>
+                }
+            >
+                <Menu.Item key="profile">
+                    <CodeOutlined /> Profile
+                </Menu.Item>
+                <Menu.Item key="personal-settings">
+                    <UserOutlined /> Personal Settings
+                </Menu.Item>
+                <Menu.Item key="log-out">
+                    <LogoutOutlined /> Logout
+                </Menu.Item>
+            </Menu.SubMenu>
+        </Menu>
+    );
+};
+
+export default RightMenu;
