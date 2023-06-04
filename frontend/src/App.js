e93562683bc64f72e27aa57ee867d528af64a4b4
SeePeaJay <cpj99@proton.me>
2023-06-04 16:52:20 -0700

    ref: move files to their appropriate location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Home from './components/Home';
+// import Thread from './components/Thread';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-
 
 // edited for thread component test
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      {/* <Thread /> */}
       <Home />
     </div>
   );
