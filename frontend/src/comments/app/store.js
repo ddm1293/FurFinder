e0be0fb4641bdc011c18c9c48d88defdf0e4f406
SeePeaJay <cpj99@proton.me>
2023-06-04 18:11:37 -0700

    ref: move files to their appropriate location

--- frontend/src/comments/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import commentReducer from "../features/commentSlice"
-
-const store = configureStore({
-    reducer: {
-        comments:commentReducer,
-    }
-});
-export default store;
