bba8d6ee0207c76aaa98e7a37f3649148080e5f3
dingmandeng <makemake1293@gmail.com>
2023-06-07 14:33:58 -0700

    yc separate contollers with routes

---
+++ src/controllers/threadController.js
@@ -0,0 +1,46 @@
+// TODO:
+export const createThread = async (req, res) => {
+  console.log('Server::createThread');
+};
+
+// TODO:
+export const getThread = async (req, res) => {
+  console.log('Server::getThread');
+};
+
+// TODO:
+export const getThreadsByUser = async (req, res) => {
+  console.log('Server::getThreadsByUser');
+};
+
+// TODO:
+export const getAllThreads = async (req, res) => {
+  try {
+    console.log('Server::getAllThreads');
+    res.status(200).json({
+
+    });
+  } catch (err) {
+
+  }
+};
+
+// TODO:
+export const updateThread = async (req, res) => {
+  console.log('Server::updateThread');
+};
+
+// TODO:
+export const patchThread = async (req, res) => {
+  console.log('Server::patchThread');
+};
+
+// TODO:
+export const archiveThread = async (req, res) => {
+  console.log('Server::archiveThread');
+};
+
+// TODO:
+export const deleteThread = async (req, res) => {
+  console.log('Server::deleteThread');
+};
---
+++ src/controllers/userController.js
@@ -0,0 +1,9 @@
+// TODO:
+export const registerUser = async (req, res) => {
+  console.log('Server::registerUser');
+};
+
+// TODO:
+export const userLogin = async (req, res) => {
+  console.log('Server::userLogin');
+};
