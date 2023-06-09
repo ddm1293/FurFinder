3b232c66337a59712840836d0118ebc606739442
dingmandeng <makemake1293@gmail.com>
2023-06-09 12:35:27 -0700

    yc configure .env

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import threadRouter from './routers/threadRouter.js';
 import userRouter from './routers/userRouter.js';
 import commentRouter from './routers/commentRouter.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
@@ -14,13 +15,17 @@ app.use('/thread', threadRouter);
 app.use('/user', userRouter);
 app.use('/comment', commentRouter);
 
-app.listen(3001, () => console.log('SERVER STARTED!!!'));
+dotenv.config();
 
-mongoose.connect('mongodb+srv://makemake1293:Oops1293!@furfinder.61gyfvo.mongodb.net/furfinder?retryWrites=true&w=majority');
+app.listen(process.env.PORT, () => console.log('SERVER STARTED!!!'));
+
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGOOSE_USERNAME}:${process.env.MONGOOSE_PASSWORD}@furfinder.61gyfvo.mongodb.net/furfinder?retryWrites=true&w=majority`
+);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log('Successfully connected to MongoDB!');
 });
