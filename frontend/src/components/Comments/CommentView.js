6e17ab1d23145ba84c31e8984678245087c03824
xuanya <xuanya@students.cs.ubc.ca>
2023-06-28 22:38:12 -0700

    refactor comment components

--- frontend/src/components/Comments/CommentView.js
+++ frontend/src/components/Comments/CommentView.js
@@ -1,11 +1,23 @@
 import '../../style/Comments.css'
 import CommentInput from './CommentInput'
 import CommentList from './CommentList'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { getCommentsAsync } from '../../thunk/commentThunk'
 
 function CommentView (props) {
   const user = useSelector((state) => state.user)
   const isLogin = user.id !== null
+  const commentList = useSelector((state) => state.comments.commentList)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getCommentsAsync(props.threadID))
+  }, [dispatch])
+
+  if (!commentList) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div className="comment-main">
@@ -15,7 +27,7 @@ function CommentView (props) {
           <div><CommentInput threadID={props.threadID} parentID={null}/></div>
         </div>
       )}
-      <CommentList threadID={props.threadID} user={user}/>
+      <CommentList threadID={props.threadID} userID={user.id} commentList={commentList}/>
     </div>
   )
 }
