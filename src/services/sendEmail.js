dd81be950976beeaa843da940e101bb116f1a8d7
xuanya <xuanya@students.cs.ubc.ca>
2023-07-27 19:26:51 -0700

    add gmail smtp

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-// export const transporter = nodemailer.createTransport({
+// const createTransporter = nodemailer.createTransport({
 //   service: 'Gmail', // Gmail SMTP
 //   auth: {
 //     user: 'your_email@example.com',
@@ -9,26 +9,22 @@ import nodemailer from 'nodemailer';
 // });
 
 const createTransporter = async () => {
-  const testAccount = await nodemailer.createTestAccount();
-  console.log('test account', testAccount);
   const transporter = nodemailer.createTransport({
-    host: testAccount.smtp.host, // 'smtp.ethereal.email',
-    port: testAccount.smtp.port,
-    secure: testAccount.smtp.secure,
+    service: 'gmail', // Gmail SMTP
     auth: {
-      user: testAccount.user,
-      pass: testAccount.pass
+      user: 'furfinder23@gmail.com',
+      pass: 'Furfinder2023@'
     }
   });
-  return { testAccount, transporter };
+  return transporter;
 };
 
 export const sendEmail = async (recipient, subject, text, html) => {
   try {
-    const { testAccount, transporter } = await createTransporter();
+    const transporter = await createTransporter();
 
     const mailOptions = {
-      from: testAccount.user,
+      from: 'furfinder23@gmail.com',
       to: recipient,
       subject: subject,
       text: text,
@@ -41,3 +37,38 @@ export const sendEmail = async (recipient, subject, text, html) => {
     console.log('Error sending email:', error.message);
   }
 };
+
+// const createTransporter = async () => {
+//   const testAccount = await nodemailer.createTestAccount();
+//   console.log('test account', testAccount);
+//   const transporter = nodemailer.createTransport({
+//     host: testAccount.smtp.host, // 'smtp.ethereal.email',
+//     port: testAccount.smtp.port,
+//     secure: testAccount.smtp.secure,
+//     auth: {
+//       user: testAccount.user,
+//       pass: testAccount.pass
+//     }
+//   });
+//   return { testAccount, transporter };
+// };
+
+//
+// export const sendEmail = async (recipient, subject, text, html) => {
+//   try {
+//     const { testAccount, transporter } = await createTransporter();
+//
+//     const mailOptions = {
+//       from: testAccount.user,
+//       to: recipient,
+//       subject: subject,
+//       text: text,
+//       html: html
+//     };
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log('Message sent: %s', info.messageId);
+//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//   } catch (error) {
+//     console.log('Error sending email:', error.message);
+//   }
+// };
