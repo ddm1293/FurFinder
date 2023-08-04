7ff254dffa897aec7eba92ee48cedf6def2d6e00
colinzuo <colinzuo@student.ubc.ca>
2023-08-04 05:33:20 -0700

    Fix the Card View cover img ratio issue.

--- frontend/src/components/Forum/CardView.js
+++ frontend/src/components/Forum/CardView.js
@@ -3,8 +3,8 @@ import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
-import { Avatar, Card } from 'antd';
-import { MessageOutlined, StarFilled, StarOutlined, UserOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+import { MessageOutlined, StarFilled, StarOutlined } from '@ant-design/icons';
 import icon from "../../static/icon.png";
 import DisplayAvatar from '../User/DisplayAvatar'
 
@@ -72,10 +72,13 @@ function CardView ({ items }) {
               <Card className="cards"
                     key={index}
                     style={{ width: 300 }}
-                    cover={<img src={getItemImgUrl(item)} alt="pet" onError={({ currentTarget }) => {
-                      currentTarget.onerror = null; // prevents looping
-                      currentTarget.src = icon;
-                    }}/>}
+                    cover={
+                    <div className="card-cover-container">
+                      <img className="card-view-img" src={getItemImgUrl(item)} alt="pet" onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = icon;
+                      }}/>
+                    </div>}
                     actions={[
                       <div onClick={() => {handleClick(item._id)}}>
                         {favourite.includes(item._id) ? <StarFilled key="star" /> : <StarOutlined key="star" />}
