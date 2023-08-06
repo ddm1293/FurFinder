3864e1e0a3346ed03741d669cd9048be6d21be9f
SeePeaJay <cpj99@proton.me>
2023-08-05 23:58:12 -0700

    fix: search bar now searches with valid URL

--- src/utils/allowedOrigins.js
+++ src/utils/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  // 'https://www.yoursite.com',
-  // 'http://localhost:3000' // default React dev server port
-  'http://localhost:3001'
+  // 'https://www.yoursite.com', // TODO: replace this w/ production url
+  'http://localhost:3000', // default React dev server port
+  'http://localhost:3001' // proxied
 ];
 
 export default allowedOrigins;
