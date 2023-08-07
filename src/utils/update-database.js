589c940e2f872ef1412c1fa57b1b6eb6cf6f9c2f
SeePeaJay <cpj99@proton.me>
2023-08-06 19:32:47 -0700

    ref: update db script now creates comments

--- src/utils/update-database.js
+++ src/utils/update-database.js
@@ -9,6 +9,17 @@ import { CommentModel } from '../../src/models/commentModel.js';
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
 
+const catApiRes = await axios.get('https://api.thecatapi.com/v1/breeds');
+const dogApiRes = await axios.get('https://api.thedogapi.com/v1/breeds');
+const catBreeds = catApiRes.data.map((breed) => ({
+  name: breed.name,
+  imageId: breed.reference_image_id
+}));
+const dogBreeds = dogApiRes.data.map((breed) => ({
+  name: breed.name,
+  imageId: breed.reference_image_id
+}));
+
 async function createComment({ commentId, content, authorId, threadId }) {
   await CommentModel.create({
     _id: commentId,
@@ -22,33 +33,66 @@ async function createComment({ commentId, content, authorId, threadId }) {
 }
 
 async function createPet(petProperties) {
+  const shouldCreatePetImage = petProperties.threadType === 'lostPetThread'
+    ? true
+    : [true, false, false][Math.floor(Math.random() * 3)];
+  let petPic;
+
+  if (shouldCreatePetImage) {
+    const petBreeds = petProperties.petSpecies === 'Cat'
+      ? catBreeds
+      : dogBreeds;
+    const petBreedId = petBreeds.find((breed) => breed.name === petProperties.petBreed).imageId;
+    const petPicUrl = petProperties.petSpecies === 'Cat'
+      ? `https://cdn2.thecatapi.com/images/${petBreedId}.jpg`
+      : `https://cdn2.thedogapi.com/images/${petBreedId}.jpg`;
+
+    try {
+      const response = await axios({ method: 'get', url: petPicUrl, responseType: 'arraybuffer' });
+      const buffer = Buffer.from(response.data, 'binary');
+
+      petPic = [{
+        data: buffer,
+        contentType: 'image/jpeg'
+      }];
+    } catch (error) {
+      if (error.response && error.response.status === 403) {
+        console.log('Forbidden, but continuing on');
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
   await PetModel.create({
     _id: petProperties.petId,
     name: petProperties.petName,
     species: petProperties.petSpecies,
     breed: petProperties.petBreed,
+    sex: petProperties.petSex,
     description: petProperties.petDescription,
     threadId: petProperties.threadId,
     threadType: petProperties.threadType,
     ownerId: petProperties.ownerId,
-    sex: petProperties.petSex,
     lastSeenTime: petProperties.lastSeenTime,
-    lastSeenLocation: petProperties.lastSeenLocation
+    lastSeenLocation: petProperties.lastSeenLocation,
+    ...(petPic && { pic: petPic })
   });
   console.log('Create pet success');
 }
 
-async function createThread(threadProperties) {
+async function createThread(threadProperties, users) {
   const petId = new mongoose.Types.ObjectId();
 
   await ThreadModel.create({
     _id: threadProperties.threadId,
-    title: `${threadProperties.titlePrefix}Lost ${threadProperties.petName} in ${threadProperties.lastSeenCity} on ${threadProperties.lastSeenDate}!${threadProperties.titleSuffix}`,
+    title: threadProperties.threadTitle,
     poster: threadProperties.poster,
     threadType: threadProperties.threadType,
     pet: petId,
-    content: `${threadProperties.petName} is a ${threadProperties.petBreed} ${threadProperties.petSpecies} that went missing on ${threadProperties.lastSeenDate} near ${threadProperties.lastSeenCity}. It is ${threadProperties.petDescription}. If you have any information about ${threadProperties.petName}'s whereabouts, please contact us. We miss it dearly and want to bring it back home safely.`,
-    comments: []
+    content: threadProperties.threadContent,
+    comments: [],
+    favoriteCount: threadProperties.threadFavoriteCount
   });
   console.log('Create thread success');
 
@@ -66,10 +110,26 @@ async function createThread(threadProperties) {
     lastSeenLocation: threadProperties.lastSeenLocation
   });
 
+  const shouldCreateComment = [true, false][Math.floor(Math.random() * 2)];
+  if (shouldCreateComment) {
+    const commentId = new mongoose.Types.ObjectId();
+    const content = [
+      'Sending positive vibes your way.',
+      'Good luck!',
+      'Wishing you the best in the search.',
+      'My thoughts are with you.',
+      'Stay strong and keep searching!'
+    ][Math.floor(Math.random() * 5)];
+    const authorId = users.map((user) => user._id)[Math.floor(Math.random() * users.length)];
+    const threadId = threadProperties.threadId;
+
+    await createComment({ commentId, content, authorId, threadId });
+  }
+
   // TODO: Perhaps refactor updatePet in case a thread document is delete without deleting the corresponding pet?
 }
 
-function randomNumber(min, max) {
+function randomCoordinate(min, max) {
   return Math.random() * (max - min) + min;
 }
 
@@ -79,134 +139,79 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
   .then(async () => {
     console.log('Successfully connected to MongoDB!');
 
-    const users = await UserModel.find({});
-    const catApiRes = await axios.get('https://api.thecatapi.com/v1/breeds');
-    const dogApiRes = await axios.get('https://api.thedogapi.com/v1/breeds');
-    const catBreeds = catApiRes.data.map((breed) => breed.name);
-    const dogBreeds = dogApiRes.data.map((breed) => breed.name);
+    await ThreadModel.deleteMany({});
+    await PetModel.deleteMany({});
+    await CommentModel.deleteMany({});
+    console.log('Reset collection success');
 
+    const userAndThreadIds = [];
+    const threadFavoriteCounts = {};
+    const users = await UserModel.find({});
     for (const user of users) {
       const threadIds = user.myThreads || [];
+      userAndThreadIds.push(...threadIds.map((threadId) => ({
+        userId: user._id,
+        threadId
+      })));
 
-      for (const threadId of threadIds) {
-        const foundThread = await ThreadModel.findById(threadId);
-
-        // new/updated properties
-        const petName = faker.person.firstName();
-        const petSpecies = ['Cat', 'Dog'][Math.floor(Math.random() * 2)];
-        const petBreed = petSpecies === 'Cat'
-          ? catBreeds[Math.floor(Math.random() * catBreeds.length)]
-          : dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
-        const petSex = ['male', 'female', 'unknown'][Math.floor(Math.random() * 3)];
-        const petDescription = `a friendly and ${faker.word.adjective()} ${petSpecies} with a ${faker.color.human()} coat`;
-        const lastSeenCity = faker.location.city();
-        const lastSeenDate = faker.date.past().toLocaleDateString();
-        const lastSeenLocation = {
-          type: 'Point',
-          coordinates: [randomNumber(-123.263754, -122.890771), randomNumber(49.241829, 49.266554)]
-        };
-        const titlePrefix = ['Help please!! ', 'Please Help! ', '救命!! ', ''][Math.floor(Math.random() * 4)];
-        const titleSuffix = [
-          ` ${Math.floor(Math.random() * 1000) + 1} furPoint wanted`,
-          ''
-        ][Math.floor(Math.random() * 2)];
-        const threadType = ['lostPetThread', 'witnessThread'][Math.floor(Math.random() * 2)];
-        const threadTitle = threadType === 'lostPetThread'
-          ? `${titlePrefix}Lost ${petName} in ${lastSeenCity} on ${lastSeenDate}!${titleSuffix}`
-          : `${titlePrefix}Saw a ${petSpecies} in ${lastSeenCity} on ${lastSeenDate}!${titleSuffix}`;
-        const threadContent = threadType === 'lostPetThread'
-          ? `${petName} is a ${petBreed} ${petSpecies} that went missing on ${lastSeenDate} near ${lastSeenCity}. It is ${petDescription}. If you have any information about ${petName}'s whereabouts, please contact us. We miss it dearly and want to bring it back home safely.`
-          : `I believe I've found a ${petBreed} ${petSpecies} on ${lastSeenDate} near ${lastSeenCity}. It is ${petDescription}. Message me if you want more info.`;
-        const threadComment = [
-          'I hope you find it soon!',
-          'Sending positive vibes your way.',
-          'Good luck!',
-          'Wishing you the best in the search.',
-          'My thoughts are with you.',
-          'Don\'t lose hope. Many lost pets are reunited with their owners.',
-          'Stay strong and keep searching!'
-        ][Math.floor(Math.random() * 7)];
-
-        if (foundThread) {
-          // delete kind, if it exists
-          foundThread.set('kind', undefined, { strict: false });
-          foundThread.save();
-
-          // update foundThread
-          await ThreadModel.findByIdAndUpdate(foundThread._id, {
-            title: threadTitle,
-            threadType,
-            content: threadContent,
-            archived: false
-          }, { overwriteDiscriminatorKey: true, new: true });
-          console.log('Update thread success');
-
-          // update or create pet
-          const foundPet = await PetModel.findById(foundThread.pet);
-          if (foundPet) {
-            await PetModel.findByIdAndUpdate(foundPet._id, {
-              name: petName,
-              species: petSpecies,
-              breed: petBreed,
-              sex: petSex,
-              description: petDescription,
-              lastSeenTime: new Date(lastSeenDate),
-              lastSeenLocation,
-              threadType
-            }, { overwriteDiscriminatorKey: true, new: true });
-            console.log('Update pet success');
-          } else {
-            await createPet({
-              petId: foundThread.pet,
-              petName,
-              petSpecies,
-              petBreed,
-              petSex,
-              petDescription,
-              threadId,
-              threadType,
-              ownerId: user._id,
-              lastSeenTime: new Date(lastSeenDate),
-              lastSeenLocation
-            });
-          }
-
-          // update comments
-          for (const commentId of foundThread.comments) {
-            const foundComment = await CommentModel.findById(commentId);
-
-            if (foundComment) {
-              await CommentModel.findByIdAndUpdate(
-                foundComment._id, { content: threadComment }
-              );
-              console.log('Update comment success');
-            } else {
-              await createComment({
-                commentId,
-                content: threadComment,
-                authorId: user._id,
-                threadId
-              });
-            }
-          }
+      const favoredThreads = user.favoredThreads || [];
+      for (const threadId of favoredThreads) {
+        if (threadId.toString() in threadFavoriteCounts) {
+          threadFavoriteCounts[threadId.toString()]++;
         } else {
-          await createThread({
-            threadId,
-            poster: user._id,
-            petName,
-            petSpecies,
-            petBreed,
-            petSex,
-            petDescription,
-            lastSeenCity,
-            lastSeenDate,
-            lastSeenLocation,
-            titlePrefix,
-            titleSuffix,
-            threadType
-          });
+          threadFavoriteCounts[threadId.toString()] = 1;
         }
       }
+    }
+
+    for (const userAndThreadId of userAndThreadIds) {
+      // new pet properties
+      const petName = faker.person.firstName();
+      const petSpecies = ['Cat', 'Dog'][Math.floor(Math.random() * 2)];
+      const petBreed = petSpecies === 'Cat'
+        ? catBreeds[Math.floor(Math.random() * catBreeds.length)].name
+        : dogBreeds[Math.floor(Math.random() * dogBreeds.length)].name;
+      const petSex = ['male', 'female', 'unknown'][Math.floor(Math.random() * 3)];
+      const petDescription = `a friendly and ${faker.word.adjective()} ${petSpecies} with a ${faker.color.human()} coat`;
+      const lastSeenCity = faker.location.city();
+      const lastSeenDate = faker.date.past().toLocaleDateString();
+      const lastSeenLocation = {
+        type: 'Point',
+        coordinates: [randomCoordinate(-123.263754, -122.890771), randomCoordinate(49.241829, 49.266554)]
+      };
+
+      // new thread properties
+      const titlePrefix = ['Help please!! ', 'Please Help! ', '救命!! ', ''][Math.floor(Math.random() * 4)];
+      const titleSuffix = [
+        ` ${Math.floor(Math.random() * 1000) + 1} furPoint wanted`,
+        ''
+      ][Math.floor(Math.random() * 2)];
+      const threadType = ['lostPetThread', 'witnessThread'][Math.floor(Math.random() * 2)];
+      const threadTitle = threadType === 'lostPetThread'
+        ? `${titlePrefix}Lost ${petName} in ${lastSeenCity} on ${lastSeenDate}!${titleSuffix}`
+        : `${titlePrefix}Saw a ${petSpecies} in ${lastSeenCity} on ${lastSeenDate}!${titleSuffix}`;
+      const threadContent = threadType === 'lostPetThread'
+        ? `${petName} is a ${petBreed} ${petSpecies} that went missing on ${lastSeenDate} near ${lastSeenCity}. It is ${petDescription}. If you have any information about ${petName}'s whereabouts, please contact us. We miss it dearly and want to bring it back home safely.`
+        : `I believe I've found a ${petBreed} ${petSpecies} on ${lastSeenDate} near ${lastSeenCity}. It is ${petDescription}. Message me if you want more info.`;
+      const threadFavoriteCount = userAndThreadId.threadId.toString() in threadFavoriteCounts
+        ? threadFavoriteCounts[userAndThreadId.threadId.toString()]
+        : 0;
+
+      await createThread({
+        threadId: userAndThreadId.threadId,
+        poster: userAndThreadId.userId,
+        petName,
+        petSpecies,
+        petBreed,
+        petSex,
+        petDescription,
+        lastSeenDate,
+        lastSeenLocation,
+        threadTitle,
+        threadType,
+        threadContent,
+        threadFavoriteCount
+      }, users);
     }
   })
   .catch((err) => {
