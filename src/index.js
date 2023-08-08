33244753a39e2a1344594c82e90a0800cdb24e5c
dingmandeng <makemake1293@gmail.com>
2023-08-08 14:33:56 -0700

    yc remove unnecessary console log

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import { createServer } from './utils/serverSetup.js';
 dotenv.config();
 
 (async () => {
-  console.log('see env: ', process.env.MONGODB_CONNECTION_STRING);
   await createServer(process.env.PORT, process.env.MONGODB_CONNECTION_STRING);
 })();
