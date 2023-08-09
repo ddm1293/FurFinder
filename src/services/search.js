e62291435d97845a2ca36a4d032b94cbf6e6a0b8
dingmandeng <makemake1293@gmail.com>
2023-08-09 10:46:14 -0700

    yc fix advanced search

--- src/services/search.js
+++ src/services/search.js
@@ -31,7 +31,10 @@ export const keywordSearch = (keyword, searchOn, threadType) => {
 export const threadTypeMatch = (threadType) => {
   return {
     $match: {
-      kind: { $regex: new RegExp(`^${threadType}$`, 'i') }
+      threadType: {
+        $regex: `^${threadType}$`,
+        $options: 'i'
+      }
     }
   };
 };
